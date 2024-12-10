bceebc166a1f9614d1f1e08d773a7d4e9976c7cc
MixhizoRR <MixhizoRR@MIXHIZOR>
2024-12-10 15:27:14 +0300

    Proje dosyası ekle.

---
+++ ExpenseCalculator/Controllers/HomeController.cs
@@ -0,0 +1,54 @@
+using ExpenseCalculator.Data;
+using ExpenseCalculator.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace ExpenseCalculator.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        private readonly ExpensesDbContext _context;
+
+        public HomeController(ILogger<HomeController> logger, ExpensesDbContext context)
+        {
+            _logger = logger;
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult Expenses()
+        {
+            var allExpenses = _context.Expenses.ToList();
+            return View(allExpenses);
+        }
+
+        public IActionResult CreateEditExpense()
+        {
+            return View();
+        }
+
+        public IActionResult CreateEditExpenseForm(Expense model)
+        {
+            _context.Expenses.Add(model);
+            _context.SaveChanges();
+            return RedirectToAction("Expenses");
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ ExpenseCalculator/Models/Expense.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ExpenseCalculator.Models
+{
+    public class Expense
+    {
+        public int ID { get; set; }
+        public decimal Value { get; set; }
+
+        [Required]
+        public string? Description { get; set; }
+    }
+}
